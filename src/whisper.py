6143042c103f278181002d438a0a3caaf2668049
nadezhdasopilova <nad59647788@gmail.com>
2023-12-21 18:47:33 +0300

    Домашнее задание 4

--- src/whisper.py
+++ src/whisper.py
@@ -9,7 +9,7 @@ def load_model():
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-    model_id = "openai/whisper-large-v3"
+    model_id = "openai/whisper-small"
 
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
         model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
